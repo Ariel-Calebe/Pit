7e5f088bd9aca30b4229f8280c9036499faab377
Ariel_Calebe <153770940+Ariel-Calebe@users.noreply.github.com>
2025-09-01 20:13:30 -0300

    exclusao

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  login: (email, senha) => ipcRenderer.invoke('login', email, senha),
-  cadastrar: (nome, email, senha) => ipcRenderer.invoke('cadastrar', nome, email, senha),
-  loginGoogle: () => ipcRenderer.invoke('login-google')
-});
